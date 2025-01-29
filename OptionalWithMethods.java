c2a9c7bb9ff5d76103fae1c1607964712080ab5d
madhu kethireddy <madhukethireddy.k@gmail.com>
2025-01-29 17:34:14 +0530

    chore: added some methods of the Optional class

--- OptionalWithMethods.java
+++ OptionalWithMethods.java
@@ -2,13 +2,16 @@ import java.util.Optional;
 
 public class OptionalWithMethods {
     public static void main(String[] args){
-        Optional<String> op = Optional.of("washington");
+        Optional<String> op = Optional.of("naresh");
 
         op.filter(name -> name.length() > 5)
         .map(name -> name.toUpperCase())
         .ifPresent(name -> System.out.println(name));
+       
+        System.out.println(op.get());
+        System.out.println(op.orElse("kethireddy"));
+        System.out.println(op.orElseGet(() -> "madhu"));
+        System.out.println(op.orElseThrow(() -> new IllegalStateException()));
 
-
-        
     }    
 }
